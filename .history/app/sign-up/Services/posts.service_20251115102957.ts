d8bb35bdaad2c14e6848775107ad6b37a36f8ca0
moshaosama <mohamedOSFekry@gmail.com>
2025-11-15 10:31:25 +0200

    Latest Version

---
+++ .history/app/sign-up/Services/posts.service_20251115102957.ts
@@ -0,0 +1,52 @@
+import { User } from "@/app/types/admin";
+import axios from "axios";
+import { headers } from "next/headers";
+
+interface CreatePostType {
+  bio: string;
+  image: string;
+  location: string;
+  tags: string[];
+  watch: string;
+  user_id: string;
+}
+
+export const GetAllPosts = async () => {
+  try {
+    const user: User = await JSON.parse(localStorage.getItem("user")!);
+
+    if (!user) {
+      console.error("User is not found");
+    }
+
+    const response = await axios.get(
+      `https://godzilla-be.vercel.app/api/v1/posts/${user?.data?.user_id}`
+    );
+    return response.data.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios error:", error.response?.data || error.message);
+    } else {
+      console.error("Unexpected error:", error);
+    }
+    return { error: "An unexpected error occurred" };
+  }
+};
+
+export const CreatePost = async (data: CreatePostType) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:4000/api/v1/posts/create-post",
+      data
+    );
+
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.error("Axios error:", error.response?.data || error.message);
+    } else {
+      console.error("Unexpected error:", error);
+    }
+    return { error: "An unexpected error occurred" };
+  }
+};
